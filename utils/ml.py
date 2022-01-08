fbd2dccf2b0eaa400ee8203acb7a48870676d774
Kate Huang <katejhuang@hotmail.com>
2022-01-07 16:00:37 -0800

    fix some types

--- utils/ml.py
+++ utils/ml.py
@@ -1,18 +1,20 @@
 from math import sqrt
 import tensorflow_hub as hub
 
-def get_vector_distance(v1: float, v2: float) -> float:
+def get_vector_distance(v1: List[float], v2: List[float]) -> float:
     distance1 = sqrt(sum(x**2 for x in v1))
     distance2 = sqrt(sum(x**2 for x in v2))
 
     return distance1 - distance2
 
-def get_phrase_and_song_similarity(phrase: str, song_id: str):
+def get_phrase_and_song_similarity(phrase: str, song_id: str) -> float:
     song_lyrics = "" # get song lyrics from scraping
 
-    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
-    phrase_vector = embed([phrase]).numpy()
-    lyrics_vector = embed([song_lyrics]).numpy()
+    embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4") # preload tensorflow model
+
+    # convert to vectors
+    phrase_vector = embed([phrase]).numpy().tolist()
+    lyrics_vector = embed([song_lyrics]).numpy().tolist()
 
     return abs(get_vector_distance(phrase_vector, lyrics_vector))
 
